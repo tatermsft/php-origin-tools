ebba31f4c75c5ff03c75bc76f1716a16972a97aa
Russell Rundell <83023898+RussellR-MSFT@users.noreply.github.com>
2022-12-30 09:13:16 -0600

    Moved styles to external stylesheet

--- index.php
+++ index.php
@@ -8,181 +8,7 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>PHP Origin Tools</title>
-    <style type="text/css" media="screen">
-  * {
-    margin: 0px 0px 0px 0px;
-    padding: 0px 0px 0px 0px;
-  }
-
-  body, html {
-    padding: 3px 3px 3px 3px;
-
-    background-color: #D8DBE2;
-
-    font-family: Verdana, sans-serif;
-    font-size: 11pt;
-    text-align: center;
-  }
-
-  div.main_page {
-    position: relative;
-    display: table;
-
-    width: 800px;
-
-    margin-bottom: 3px;
-    margin-left: auto;
-    margin-right: auto;
-    padding: 0px 0px 0px 0px;
-
-    border-width: 2px;
-    border-color: #212738;
-    border-style: solid;
-
-    background-color: #FFFFFF;
-
-    text-align: center;
-  }
-  div.page_header {
-    height: 99px;
-    width: 100%;
-
-    background-color: #F5F6F7;
-  }
-
-  div.page_header span {
-    margin: 15px 0px 0px 50px;
-
-    font-size: 180%;
-    font-weight: bold;
-  }
-
-  div.page_header img {
-    margin: 3px 0px 0px 40px;
-
-    border: 0px 0px 0px;
-  }
-
-  div.table_of_contents {
-    clear: left;
-
-    min-width: 200px;
-
-    margin: 3px 3px 3px 3px;
-
-    background-color: #FFFFFF;
-
-    text-align: left;
-  }
-
-  div.table_of_contents_item {
-    clear: left;
-
-    width: 100%;
-
-    margin: 4px 0px 0px 0px;
-
-    background-color: #FFFFFF;
-
-    color: #000000;
-    text-align: left;
-  }
-    div.table_of_contents_item a {
-    margin: 6px 0px 0px 6px;
-  }
-
-  div.content_section {
-    margin: 3px 3px 3px 3px;
-
-    background-color: #FFFFFF;
-
-    text-align: left;
-  }
-
-  div.content_section_text {
-    padding: 4px 8px 4px 8px;
-
-    color: #000000;
-    font-size: 100%;
-  }
-
-  div.content_section_text pre {
-    margin: 8px 0px 8px 0px;
-    padding: 8px 8px 8px 8px;
-
-    border-width: 1px;
-    border-style: dotted;
-    border-color: #000000;
-
-    background-color: #F5F6F7;
-
-    font-style: italic;
-  }
-
-  div.content_section_text p {
-    margin-bottom: 6px;
-  }
-
-  div.content_section_text ul, div.content_section_text li {
-    padding: 4px 8px 4px 16px;
-  }
-
-  div.section_header {
-    padding: 3px 6px 3px 6px;
-    background-color: #8E9CB2;
-    color: #FFFFFF;
-    font-weight: bold;
-    font-size: 112%;
-    text-align: center;
-  }
-
-  div.section_header_red {
-    background-color: #CD214F;
-  }
-
-  div.section_header_grey {
-    background-color: #9F9386;
-  }
-
-  .floating_element {
-    position: relative;
-    float: left;
-  }
-
-  div.table_of_contents_item a,
-  div.content_section_text a {
-    text-decoration: none;
-    font-weight: bold;
-  }
-
-  div.table_of_contents_item a:link,
-  div.table_of_contents_item a:visited,
-  div.table_of_contents_item a:active {
-    color: #000000;
-  }
-
-  div.table_of_contents_item a:hover {
-    background-color: #000000;
-
-    color: #FFFFFF;
-  }
-
-  div.content_section_text a:link,
-  div.content_section_text a:visited,
-   div.content_section_text a:active {
-    background-color: #DCDFE6;
-
-    color: #000000;
-  }
-    div.content_section_text a:hover {
-    background-color: #000000;
-
-    color: #DCDFE6;
-  }
-
-  div.validator {
-  }
-    </style>
+    <link rel="stylesheet" href="styles.css">
   </head>
   <body>
     <div class="main_page">
@@ -193,27 +19,7 @@
           <?php echo date(DATE_RFC2822);?>
         </span>
       </div>
-<!--      <div class="table_of_contents floating_element">
-        <div class="section_header section_header_grey">
-          TABLE OF CONTENTS
-        </div>
-        <div class="table_of_contents_item floating_element">
-          <a href="#about">About</a>
-        </div>
-        <div class="table_of_contents_item floating_element">
-          <a href="#headers">Headers</a>
-        </div>
-        <div class="table_of_contents_item floating_element">
-          <a href="#scope">Scope</a>
-        </div>
-        <div class="table_of_contents_item floating_element">
-          <a href="#files">Config files</a>
-        </div>
-      </div>
--->
       <div class="content_section floating_element">
-
-
         <div class="section_header section_header_red">
           <div id="about"></div>
           Host name: <?php echo gethostname(); ?>
@@ -224,15 +30,14 @@
             troubleshooting application layer issues by providing visibility into the headers seen on the backend or origin behind various
             networking load balancers such as Azure Application Gateways, Content Delivery Network endpoints, or Azure Front Doors.
           </p>
-
         </div>
-<!-- From https://www.php.net/manual/en/function.apache-request-headers.php -->
         <div class="section_header">
             <div id="requestheaders"></div>
             Request Headers
             </div>
         <div class="content_section_text">
           <?php
+           # From https://www.php.net/manual/en/function.apache-request-headers.php
            $requestHeaders = apache_request_headers();
 
            foreach ($requestHeaders as $header => $value) {
@@ -240,13 +45,15 @@
            }
           ?>
         </div>
-<!-- From https://stackoverflow.com/questions/196664/print-out-post-values -->
         <div class="section_header">
             <div id="postvalues"></div>
             POST Values
             </div>
         <div class="content_section_text">
-          <?php echo "<pre>"; print_r($_POST) ;  echo "</pre>";  ?>
+          <?php 
+           # From https://stackoverflow.com/questions/196664/print-out-post-values
+           echo "<pre>"; print_r($_POST) ;  echo "</pre>";  
+          ?>
         </div>
       </div>
     </div>
